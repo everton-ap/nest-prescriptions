e2344f8ec21b6fef8502f8238134319ed5aefbb2
everton.araujo <everton.araujo@staycharlie.com.br>
2025-11-12 22:13:10 -0300

    feat: add prescription entity

--- src/prescriptions/entities/prescription.entity.ts
+++ src/prescriptions/entities/prescription.entity.ts
@@ -1,1 +1,34 @@
-export class Prescription {}
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class Prescription {
+  @PrimaryColumn({ unique: true })
+  id: string;
+
+  @Column()
+  date: string;
+
+  @Column()
+  patienteCpf: string;
+
+  @Column()
+  doctorUf: string;
+
+  @Column()
+  medication: string;
+
+  @Column()
+  controlled: boolean;
+
+  @Column()
+  dosage: string;
+
+  @Column()
+  frequency: string;
+
+  @Column()
+  duration: number;
+
+  @Column()
+  notes: string;
+}
